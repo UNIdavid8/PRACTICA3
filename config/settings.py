a8cbdb4463b37a3e0f2b3d5853c89a44e440a113
UNIdavid8 <dmurigar@myuax.com>
2026-04-24 12:05:44 +0200

    Fix DB settings (sqlite file), migrations, and complete Practica 3 integration

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',  # Base de datos volátil en RAM
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
